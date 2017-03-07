9b89acdbbaff3f0ef996f1bf81cf5a45265b7b5b
ttwr <aleksey143796@gmail.com>
2017-03-07 13:43:36 +0700

    реализовал классы предметной области

---
+++ Java/model/Avtomobil.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.awt.Color;
+import java.util.Date;
+
+
+public class Avtomobil {
+    
+    Integer id;
+    String marka;
+    Proizvoditel proizvoditel;
+    Tip_kyzova tip_kyzova;
+    Date proizvodstvo;
+    Integer color;
+    
+            
+    
+    
+    
+}
